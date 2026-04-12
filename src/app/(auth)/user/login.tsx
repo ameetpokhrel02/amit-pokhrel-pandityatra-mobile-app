2c6adfef76bed028cf661b0b794490c560a22eb3
pokhrelameet12@gmail.com <pokhrelameet12@gmail.com>
2026-04-12 17:51:49 +0545

    Fix duplicate handleGooglePress identifier

--- src/app/(auth)/user/login.tsx
+++ src/app/(auth)/user/login.tsx
@@ -43,7 +43,6 @@ export default function LoginScreen() {
     handleSendOtp,
     handlePasswordLogin,
     handleGooglePress,
-    handleGooglePress,
     navToSignup,
     exploreAsGuest,
     otpCode,
